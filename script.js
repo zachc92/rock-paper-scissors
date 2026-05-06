f89747a966d314c051c24ae77e3f013c972d6586
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-06 17:40:13 -0400

    Add UI to rps game

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-const people = [
-  "Chris",
-  "Anne",
-  "Colin",
-  "Terri",
-  "Phil",
-  "Lola",
-  "Sam",
-  "Kay",
-  "Bruce",
-];
+let rockButton = document.querySelector('.rockButton');
+let paperButton = document.querySelector('.paperButton');
+let scissorsButton = document.querySelector('.scissorsButton');
 
-const admitted = document.querySelector(".admitted");
-const refused = document.querySelector(".refused");
-admitted.textContent = "Admit: ";
-refused.textContent = "Refuse: ";
+let computerScore = 0;
+let humanScore = 0;
 
-// loop starts here
-
-for (const person of people){
-    if(person == "Phil" || person == "Lola"){
-        refused.textContent += `${person}, `;
-    } else {
-        admitted.textContent += `${person}, `;
+function playRound(e){
+    console.log(e.target.textContent);
+    function getHumanChoice() {
+        let choice = e.target.textContent
+        return choice;
+    }
+    function getComputerChoice() {
+        let number = Math.random() * 10;
+        if (number < 3){
+            return "rock";
+        } else if (number <= 6){
+            return "paper";
+        } else if (number <= 10){
+            return "scissors"
+        }
+    }
+    let human = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    let humanChoice = human.toLowerCase();
+    if (humanChoice === computerChoice){
+        document.querySelector('.roundOutcome').innerText = `You chose ${humanChoice}, the computer chose ${computerChoice}. It's a tie!`;
+        document.querySelector('#playerScore').innerText = humanScore;
+        document.querySelector('#computerScore').innerText = computerScore;
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'rock'){
+        document.querySelector('.roundOutcome').innerText = `You chose ${humanChoice}, the computer chose ${computerChoice}. You win!`;
+        humanScore++;
+        document.querySelector('#playerScore').innerText = humanScore;
+        document.querySelector('#computerScore').innerText = computerScore;
+    } else if (computerChoice == 'rock' && humanChoice == 'scissors' || computerChoice == 'scissors' && humanChoice == 'paper' || computerChoice == 'paper' && humanChoice == 'rock'){
+        document.querySelector('.roundOutcome').innerText = `You chose ${humanChoice}, the computer chose ${computerChoice}. You lose!`;
+        computerScore++;
+        document.querySelector('#playerScore').innerText = humanScore;
+        document.querySelector('#computerScore').innerText = computerScore;   
+    }
+    if (humanScore == 5) {
+        document.querySelector('.roundOutcome').innerText = `Game over, you win! Player Score: ${humanScore} Computer Score: ${computerScore}`;
+        humanScore = 0;
+        computerScore = 0;
+        document.querySelector('#playerScore').innerText = humanScore;
+        document.querySelector('#computerScore').innerText = computerScore;
+    } else if (computerScore == 5){
+        document.querySelector('.roundOutcome').innerText = `Game over, you lose! Computer Score: ${computerScore} Player Score: ${humanScore}`;
+        humanScore = 0;
+        computerScore = 0;
+        document.querySelector('#playerScore').innerText = humanScore;
+        document.querySelector('#computerScore').innerText = computerScore;
     }
 }
 
-let finalRefusedList = refused.textContent.slice(0, refused.textContent.length-2);
-let finalAdmittedList = admitted.textContent.slice(0, admitted.textContent.length-2);
-
-refused.textContent = finalRefusedList;
-admitted.textContent = finalAdmittedList;
+rockButton.addEventListener('click', playRound);
+paperButton.addEventListener('click', playRound);
+scissorsButton.addEventListener('click', playRound);
